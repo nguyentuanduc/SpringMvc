7404ba4ddec124a78c9bc4563de4a7b061cef45a
nguyentuanduc <nguyentuanduc18@gmai.com>
2019-04-24 00:23:55 +0700

    add to develop

--- SpringMvc/src/main/java/com/spring/app/App.java
+++ SpringMvc/src/main/java/com/spring/app/App.java
@@ -3,7 +3,10 @@ package com.spring.app;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello the digital world");
+		
+		System.out.println("test");
 	}
 
+	
 }
